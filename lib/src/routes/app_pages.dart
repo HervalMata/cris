5c0060b0cafd0767c06b5c773b6dbe17f0843de9
HervalMata <hervalmataamparo@gmail.com>
2023-07-20 01:43:27 -0300

    feat:art: mobile - create app page file and added get

---
+++ lib/src/routes/app_pages.dart
@@ -0,0 +1,8 @@
+import 'package:cris/src/routes/app_routes.dart';
+import 'package:get/get.dart';
+
+class AppPages {
+  static const initial = AppRoutes.introRoute;
+
+  static final pages = <GetPage>[];
+}
